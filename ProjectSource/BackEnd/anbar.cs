3550226a55db9405d7729081b168dcfc7ad8dfdc
Ladan Ghasemi <75240852+ladan-gh@users.noreply.github.com>
2021-07-03 00:06:37 +0430

    Add files via upload

---
+++ ProjectSource/BackEnd/anbar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LadanANDAsma.entity
+{
+    public class  anbar
+    {
+        public int inventoryID { get; set; }
+        public String inventoryName { get; set; }
+
+    }
+}
